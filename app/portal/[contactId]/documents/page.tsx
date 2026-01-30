a8fd584451d8ec56336ed47c750561c7445510e7
YHSG-Kling <dkling@theklinggroup.com>
2026-01-29 20:34:34 -0600

    Add 'use client' directive to page.tsx
    Signed-off-by: YHSG-Kling <dkling@theklinggroup.com>

--- app/portal/[contactId]/documents/page.tsx
+++ app/portal/[contactId]/documents/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { createClient } from "@/lib/supabase/server"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
